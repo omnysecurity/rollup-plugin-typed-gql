03016f66fd7773af51415d0235233910a29b4e89
Boye Borg Nygård <6630430+boyeborg@users.noreply.github.com>
2023-05-07 10:54:09 +0200

    rename

--- src/lib/index.js
+++ src/lib/index.js
@@ -43,7 +43,7 @@ export default async function vitePluginTypedGql() {
 	};
 
 	return {
-		name: "vite-plugin-typed-gql",
+		name: "rollup-plugin-typed-gql",
 
 		async buildStart() {
 			schema = await loadSchema(schemaPath);
